4a6d110f328dd452a80678978ce5b52c477f2225
JuanPGarzonC <juanpablogarzoncub@gmail.com>
2022-04-15 12:36:04 -0500

    Db script finished

--- Docs/Db Script/tododb.sql
+++ Docs/Db Script/tododb.sql
@@ -20,3 +20,9 @@ create table ToDoDB.ToDoList (
     primary key(id),
     foreign key(User_idUser) references ToDoDB.User (id)
 );
+
+alter table ToDoDB.User modify column name varchar(225) not null;
+alter table ToDoDB.User modify column lastName varchar(225) not null;
+alter table ToDoDB.User modify column email varchar(225) not null unique;
+alter table ToDoDB.User modify column password varchar(20) not null;
+alter table ToDoDB.User add primary key(User_idUser);
